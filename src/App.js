e9bfc1ba937500683c794909e068bfa69536b2cb
Sunny Rathaur <sunny.rathaur@jetsetgo.in>
2024-05-14 18:05:44 +0530

    add new pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './components/Home';
-import Contact from './components/Contact';
 import About from './components/About';
+import Contact from './components/Contact';
+import Products from './components/Products';
+import Cart from './components/Cart';
+import SingleProduct from './components/SingleProduct';
+import ErrorPage from './components/ErrorPage'
 
 const App = () => {
   return (
@@ -10,8 +14,12 @@ const App = () => {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Home />} />
-          <Route path='/contact-us' element={<Contact />} />
           <Route path='/about-us' element={<About />} />
+          <Route path='/contact-us' element={<Contact />} />
+          <Route path='/products' element={<Products/>} />
+          <Route path='/cart' element={<Cart/>} />
+          <Route path='/singleproduct/:id' element={<SingleProduct />} />
+          <Route path='*' element={<ErrorPage/>} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/components/ErrorPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ErrorPage = () => {
+  return (
+    <>
+      <div>
+        <h1>404</h1>
+        <h2>ErrorPage</h2>
+      </div>
+    </>
+  )
+}
+
+export default ErrorPage
