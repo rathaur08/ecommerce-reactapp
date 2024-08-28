d07acdd8054cc852024b59d0c0ce17f0fab12a91
Sunny Rathaur <sunny.rathaur@jetsetgo.in>
2024-08-28 15:46:50 +0530

    add/Update Input Range Price Filter

--- src/components/FilterSection.js
+++ src/components/FilterSection.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { useFilterContext } from './context/FilterContext';
 import { FaCheck } from "react-icons/fa";
+import FormatPrice from './Helper/FormatPrice';
 
 const FilterSection = () => {
-  const { all_products, filters: { text, category, company, color }, updateFilterValue } = useFilterContext();
+  const { all_products, filters: { text, category, company, color, maxPrice, price, minPrice }, updateFilterValue } = useFilterContext();
 
   //  to get the UNIQUE data of each FiELDS
   const getUniqueData = (data, property) => {
@@ -26,8 +27,6 @@ const FilterSection = () => {
   const categoryOnlyData = getUniqueData(all_products, "category");
   const companyOnlyData = getUniqueData(all_products, "company");
   const colorOnlyData = getUniqueData(all_products, "colors");
-  const priceOnlyData = getUniqueData(all_products, "colors");
-
   console.log("colorOnlyData.. " + colorOnlyData)
 
   return (
@@ -82,7 +81,12 @@ const FilterSection = () => {
       </div>
       <div className='filter-price'>
         <h3 className='mt-3'>price</h3>
+        <p className=""> <FormatPrice price={price} /></p>
+        <div>
+          <input type="range" name='price' min={minPrice} max={maxPrice} value={price} onChange={updateFilterValue} />
+          {/* <label for="cowbell">Cowbell</label> */}
         </div>
+      </div>
     </>
   )
 }
--- src/components/reducer/FilterReducer.js
+++ src/components/reducer/FilterReducer.js
@@ -2,22 +2,21 @@ const filterReducer = (state, action) => {
 
   switch (action.type) {
 
-
     case "LOAD_FILTER_PRODUCTS":
 
       let priceArr = action.payload.map((curElem) => curElem.price);
       console.log("price Array" + priceArr);
 
       // step 1 get high priceArr value 
-      console.log("maxPrice value 1 " + Math.max.apply(null, priceArr));
+      // console.log("maxPrice value 1 " + Math.max.apply(null, priceArr));
 
       // step 2 get high priceArr value 
-      let maxPrice = priceArr.reduce((initalval, curVal) => Math.max(initalval, curVal), 0);
-      console.log("maxPrice value 2 " + maxPrice);
+      // let maxPrice = priceArr.reduce((initalval, curVal) => Math.max(initalval, curVal), 0);
+      // console.log("maxPrice value 2 " + maxPrice);
 
       // step 3 get high priceArr value
-      let maxPrice3 = Math.max(...priceArr);
-      console.log("maxPrice value 3 ", maxPrice3);
+      let maxPrice = Math.max(...priceArr);
+      console.log("maxPrice value 3 ", maxPrice);
 
       return {
         ...state,
@@ -94,7 +93,7 @@ const filterReducer = (state, action) => {
       let { all_products } = state;
       let tempFilterProduct = [...all_products];
 
-      const { text, category, company, color } = state.filters;
+      const { text, category, company, color, price } = state.filters;
       if (text) {
         tempFilterProduct = tempFilterProduct.filter((curElen) => {
           return curElen.name.toLowerCase().includes(text);
@@ -117,6 +116,16 @@ const filterReducer = (state, action) => {
       if (color !== "all") {
         tempFilterProduct = tempFilterProduct.filter(
           (curElem) => curElem.colors.includes(color)
+        );
+      }
+
+      if (price === 0) {
+        tempFilterProduct = tempFilterProduct.filter(
+          (curElem) => curElem.price === price
+        );
+      } else {
+        tempFilterProduct = tempFilterProduct.filter(
+          (curElem) => curElem.price <= price
         );
       }
 
