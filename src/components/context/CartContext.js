f23ddcc65b645150ff7a17b59ec1113f40bf5d83
Sunny Rathaur <sunny.rathaur@jetsetgo.in>
2024-08-31 17:47:44 +0530

    use LocalStorage to Store and Retrieve Shopping Cart items

--- src/components/context/CartContext.js
+++ src/components/context/CartContext.js
@@ -1,10 +1,21 @@
-import { createContext, useContext, useReducer } from "react";
+import { createContext, useContext, useReducer, useEffect } from "react";
 import reducer from "../reducer/CartReducer"
 
 const CartContext = createContext();
 
+const getLocalCartData = () => {
+  let localCartData = localStorage.getItem("ecomCart")
+
+  if (localCartData === []) {
+    return [];
+  } else {
+    return JSON.parse(localCartData);
+  }
+}
+
 const initialState = {
-  cart: [],
+  // cart: [],
+  cart: getLocalCartData(),
   total_item: "",
   total_amount: "",
   shipping_fee: 50000,
@@ -17,9 +28,18 @@ const CartProvider = ({ children }) => {
     dispatch({ type: "ADD_TO_CART", payload: { id, amount, color, product } })
   };
 
+  // to remove the indivisual item from cart
   const removeItem = (id) => {
     dispatch({ type: "REMOVE_ITEM", payload: id })
   }
+
+  // to add cart item in local storage
+  // get vs set
+
+  useEffect(() => {
+    localStorage.setItem("ecomCart", JSON.stringify(state.cart))
+  }, [state.cart]);
+
 
   return (
     <CartContext.Provider value={{ ...state, addTOCart, removeItem }}>
