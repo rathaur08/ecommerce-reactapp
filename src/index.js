87a5784d55f31944b86ac6352c775e7a47ce9817
Sunny Rathaur <sunny.rathaur@jetsetgo.in>
2024-09-03 17:53:26 +0530

    Protect your website Personal/ API TOKEN Data with DOTENV

--- src/index.js
+++ src/index.js
@@ -9,10 +9,14 @@ import { CartProvider } from './components/context/CartContext';
 import { Auth0Provider } from '@auth0/auth0-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const domain = process.env.REACT_APP_AUTH_DOMAIN;
+const clientId = process.env.REACT_APP_CLIENT_ID
+
 root.render(
   <Auth0Provider
-    domain="dev-8thz7ce7804yyioy.us.auth0.com"
-    clientId="gndTnvbjQAazydfA7WLB6iSnz5PS8k7q"
+    domain={domain}
+    clientId={clientId}
     authorizationParams={{
       redirect_uri: window.location.origin
     }}>
